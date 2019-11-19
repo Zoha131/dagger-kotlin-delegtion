97e6c86edfd860dd908585001ee7ea668bd2e1df
zoha <zoha@bylc.org>
2019-11-19 15:27:20 +0600

    added code to reproduce dagger kotlin class delegation issue to share in stackoverflow

---
+++ app/src/main/java/io/github/zoha131/daggerkotlindelegate/MainActivity.kt
@@ -0,0 +1,35 @@
+package io.github.zoha131.daggerkotlindelegate
+
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
+
+@Component
+interface ActivityComponent {
+
+    @Component.Factory
+    interface Factory{
+        fun create(
+            @BindsInstance pref: SharedPreferences
+        ): ActivityComponent
+    }
+
+    fun getClient(): PrefClient
+}
+
+class PrefClient @Inject constructor(pref: SharedPreferences): SharedPreferences by pref{
+
+}
